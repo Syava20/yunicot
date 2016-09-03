f076d7d9331285cd6860f5c1204742e0808da0e9
yunicot <syava05.96@bk.ru>
2016-09-03 16:40:57 +0300

    Added russian localisation

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль должен быть длинее 6 символов и соответствоавать с полем подтвержденмя',
+    'reset' => 'Ваш парольль был сюрошен!',
+    'sent' => 'Вам отправлена ссылка для сброса пароля!',
+    'token' => 'Код сброса пароля указан неверно!',
+    'user' => "Пользователь с данным email не зарегистрирован!",
+
+];
