46581e3de8211ef8defe1681bbb36f163d369cfb
yunicot <syava05.96@bk.ru>
2016-10-02 20:53:13 +0300

    Changed 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Yunicot - 404</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
 <p>You broke the balance of the internet</p>
-</body>
-</html>
+@endsection
